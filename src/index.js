d1a0db1742a61cfe878fb99c2511390f0f48b516
abdikasa <abdikasa@sheridancollege.ca>
2020-08-25 16:37:15 -0400

    1.4 Bonus:: Use array as a prop and use components

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// 1.4: course information step4
+// 1.4 Extra: course information step4
 // And then place the objects into an array. Modify the variable definitions of App into the following form and modify the other parts of the application accordingly:
 
 const App = () => {
@@ -23,22 +23,9 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {parts[0].name}: {parts[0].exercises}
-      </p>
-      <p>
-        {parts[1].name}: {parts[1].exercises}
-      </p>
-      <p>
-        {parts[2].name}: {parts[2].exercises}
-      </p>
-      <p>
-        Total number of exercises:
-        {parts.reduce((acc, curr) => {
-          return (acc += curr.exercises);
-        }, 0)}
-      </p>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   );
 };
