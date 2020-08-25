8587eb5595ca668cd1a658d92c0e9c0913be496c
abdikasa <abdikasa@sheridancollege.ca>
2020-08-25 16:26:03 -0400

    Finished ecercise 1.3; deleted other components and stored each part as an object.

--- src/index.js
+++ src/index.js
@@ -1,54 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// 1.2: course information, step2
-// Refactor the Content component so that it does not render any names of parts or their number of exercises by itself. Instead it only renders three Part components of which each renders the name and number of exercises of one part.
-
-const Header = (prop) => (
-  <>
-    <h1>I am enrolled in the {prop.course} program.</h1>
-  </>
-);
-
-const Part = (prop) => {
-  return (
-    <>
-      <p>
-        {prop.title}: {prop.total}
-      </p>
-    </>
-  );
-};
-
-const Content = () => {
-  return (
-    <>
-      <Part title="Fundamentals of React" total="10" />
-      <Part title="Using props to pass data" total="7" />
-      <Part title="State of a component" total="14" />
-    </>
-  );
-};
-
-const Total = (prop) => {
-  const sum = prop.total.reduce((acc, curr) => {
-    return (acc += curr);
-  }, 0);
-  return (
-    <>
-      <p>Total number of credits: {sum}</p>
-    </>
-  );
-};
+// 1.3: course information step3
+// Let's move forward to using objects in our application. Modify the variable definitions of the App component as follows and also refactor the application so that it still works:
 
 const App = () => {
   const course = "Half Stack application development";
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10,
+  };
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7,
+  };
+  const part3 = {
+    name: "State of a component",
+    exercises: 14,
+  };
+
   return (
-    <>
-      <Header course={course} />
-      <Content />
-      <Total total={[10, 7, 14]} />
-    </>
+    <div>
+      <h1>{course}</h1>
+      <p>
+        {part1.name} {part1.exercises}
+      </p>
+      <p>
+        {part2.name} {part2.exercises}
+      </p>
+      <p>
+        {part3.name} {part3.exercises}
+      </p>
+      <p>
+        Total number of ecerxises are:{" "}
+        {part1.exercises + part2.exercises + part3.exercises}
+      </p>
+    </div>
   );
 };
 
