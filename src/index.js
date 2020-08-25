e68d783733a60c8f1d2ad185e22828099359152d
abdikasa <abdikasa@sheridancollege.ca>
2020-08-25 16:53:28 -0400

    Starting exercise 1.5: Change the course and its parts into a single JavaScript object.

--- src/index.js
+++ src/index.js
@@ -1,65 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// 1.4 Extra: course information step4
-// And then place the objects into an array. Modify the variable definitions of App into the following form and modify the other parts of the application accordingly:
-
-const Header = (prop) => {
-  return <h1>{prop.title}</h1>;
-};
-const Content = (prop) => {
-  return (
-    <>
-      <p>
-        {prop.parts[0].name} : {prop.parts[0].exercises}
-      </p>
-      <p>
-        {prop.parts[1].name} : {prop.parts[1].exercises}
-      </p>
-      <p>
-        {prop.parts[2].name} : {prop.parts[2].exercises}
-      </p>
-    </>
-  );
-};
-
-const Total = (prop) => {
-  return (
-    <>
-      <p>
-        Total number of exercises:{" "}
-        {prop.parts.reduce((a, b) => {
-          return (a += b.exercises);
-        }, 0)}
-      </p>
-    </>
-  );
-};
+// 1.5: course information step5
+// Let's take the changes one step further. Change the course and its parts into a single JavaScript object. Fix everything that breaks.
 
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
-  return (
-    <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
-    </div>
-  );
+  return <div>...</div>;
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
