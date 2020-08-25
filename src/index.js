f852062778c58ca14e812183661dea4be2256b6a
abdikasa <abdikasa@sheridancollege.ca>
2020-08-25 16:27:42 -0400

    Starting exercise 1.4, use an array instead of an object literal

--- src/index.js
+++ src/index.js
@@ -1,41 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// 1.3: course information step3
-// Let's move forward to using objects in our application. Modify the variable definitions of the App component as follows and also refactor the application so that it still works:
+// 1.4: course information step4
+// And then place the objects into an array. Modify the variable definitions of App into the following form and modify the other parts of the application accordingly:
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
-  };
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
 
   return (
-    <div>
-      <h1>{course}</h1>
-      <p>
-        {part1.name} {part1.exercises}
-      </p>
-      <p>
-        {part2.name} {part2.exercises}
-      </p>
-      <p>
-        {part3.name} {part3.exercises}
-      </p>
-      <p>
-        Total number of ecerxises are:{" "}
-        {part1.exercises + part2.exercises + part3.exercises}
-      </p>
-    </div>
+    
   );
 };
 
