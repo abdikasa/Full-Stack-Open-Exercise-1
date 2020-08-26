8e200ba5cd09e2e617ab76cde162b8bd826dbec9
abdikasa <abdikasa@sheridancollege.ca>
2020-08-25 20:25:17 -0400

    practicing with click events; increment and reset the count on click.

--- src/index.js
+++ src/index.js
@@ -1,46 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-// 1.5: course information step5
-// Let's take the changes one step further. Change the course and its parts into a single JavaScript object. Fix everything that breaks.
-
 const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-      },
-    ],
-  };
+  //start at 0, use deconstruction
+  const [counter, setCounter] = useState(0);
+
+  const increment = () => setCounter(counter + 1);
+  const reset = () => setCounter(0);
 
   return (
     <div>
-      <h1>{course.name}</h1>
-      <p>
-        {course.parts[0].name} : {course.parts[0].exercises}
-      </p>
-      <p>
-        {course.parts[1].name} : {course.parts[1].exercises}
-      </p>
-      <p>
-        {course.parts[2].name} : {course.parts[2].exercises}
-      </p>
-      <p>
-        Total exercises :{" "}
-        {course.parts.reduce((a, b) => {
-          return (a += b.exercises);
-        }, 0)}
-      </p>
+      <div>{counter}</div>
+      <button onClick={increment}>plus</button>
+      <button onClick={reset}>reset</button>
     </div>
   );
 };
